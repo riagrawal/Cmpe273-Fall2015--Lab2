c1f389fe63c0464b4b306f03b8322222a772e725
unknown <richa.agrawal@sjsu.edu>
2015-10-21 14:49:39 -0700

    lab2

---
+++ lab2.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+    "github.com/julienschmidt/httprouter"
+    "net/http"
+    "encoding/json"
+    "io/ioutil"
+  )
+  
+type req_struct struct{
+       Name string
+}
+
+type output struct{
+
+  Name    string       `json:"greeting"`
+}
+
+func post(rw http.ResponseWriter, req *http.Request, p httprouter.Params) {
+    body, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+        return
+    }
+   var t req_struct  
+   err = json.Unmarshal(body, &t)
+    if (err != nil || t.Name == "") {
+        http.Error(rw, "Bad Request, check request payload", http.StatusBadRequest)
+        return
+    }
+    var o output
+    o.Name = "Hello, "+t.Name+"!"
+  payload, err := json.Marshal(o)  
+  if err != nil {
+     http.Error(rw,"Bad Request, check request payload" , http.StatusInternalServerError)
+     return
+  }
+  rw.Header().Set("Content-Type", "application/json")
+  rw.Write(payload)
+}
+
+func main() {
+    mux := httprouter.New()
+    mux.POST("/hello",post)
+    server := http.Server{
+            Addr:        "0.0.0.0:8080",
+            Handler: mux,
+    }
+    server.ListenAndServe()
+}
